465a3f4268023047714c57dcdc0a23648842103a
Arnau Siches <asiches@gmail.com>
2010-05-30 15:28:23 +0200

    CHANGE factor method to public

--- lib/iso8601/atoms.rb
+++ lib/iso8601/atoms.rb
@@ -9,6 +9,9 @@ module ISO8601
     def to_i
       @atom
     end
+    def to_seconds
+      @atom.to_f * self.factor
+    end
 
     private
       def is_integer?(arg, error_message=nil)
@@ -18,47 +21,44 @@ module ISO8601
           raise RangeError, error_message
         end
       end
-      def factor(f)
-        # factor to calculate conversions
-        if @base.nil?
-          f
-        else
-          raise TypeError, "Base date not yet implemented."
-        end
-      end
   end
 
   class Years < ISO8601::Atom
-    def to_seconds
-      @atom.to_f * factor(((365 * 303 + 366 * 97) / 400) * 86400)
+    def factor
+      if @base.nil?
+        ((365 * 303 + 366 * 97) / 400) * 86400
+      else
+        raise TypeError, "Base date not yet implemented."
+      end
     end
-    
   end
-
   class Months < ISO8601::Atom
-    def to_seconds
-      @atom.to_f * factor((((365 * 303 + 366 * 97) / 400) * 86400) / 12)
+    def factor
+      if @base.nil?
+        (((365 * 303 + 366 * 97) / 400) * 86400) / 12
+      else
+        raise TypeError, "Base date not yet implemented."
+      end
     end
   end
-
   class Days < ISO8601::Atom
-    def to_seconds
-      @atom.to_f * factor(86400)
+    def factor
+      86400
     end
   end
   class Hours < ISO8601::Atom
-    def to_seconds
-      @atom.to_f * factor(3600)
+    def factor
+      3600
     end
   end
   class Minutes < ISO8601::Atom
-    def to_seconds
-      @atom.to_f * factor(60)
+    def factor
+      60
     end
   end
   class Seconds < ISO8601::Atom
-    def to_seconds
-      @atom.to_f * factor(1)
+    def factor
+      1
     end
   end
 end
