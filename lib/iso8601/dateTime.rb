b8bb6d323658a73b6d1e0be46b584a019bad76c0
Arnau Siches <asiches@gmail.com>
2010-05-30 15:26:07 +0200

    CHANGE variable names to underscore separator pattern

--- lib/iso8601/dateTime.rb
+++ lib/iso8601/dateTime.rb
@@ -1,14 +1,14 @@
 module ISO8601
   class DateTime
-    attr_reader :dateTime, :year, :month, :day, :hour, :minute, :second, :timezone
-    def initialize(dateTime)
+    attr_reader :date_time, :century, :year, :month, :day, :hour, :minute, :second, :timezone
+    def initialize(date_time)
       @dt = /^(?:
-                (\d{4})
+                (\d{2})(\d{2})?
                 (?:
                   (-)?(\d{2})
                 )?
                 (?:
-                  (\2)?(\d{2})
+                  (\3)?(\d{2})
                 )?
               )?
               (?:
@@ -17,60 +17,62 @@ module ISO8601
                   (:)?(\d{2})
                 )?
                 (?:
-                  (\7)?(\d{2})
+                  (\8)?(\d{2})
                 )?
                 (
                   Z|([+-])
                     (\d{2})
                     (?:
-                      (\7)?
+                      (\8)?
                       (\d{2})
                     )?
                 )?
               )?
-            $/x.match(dateTime) or raise ISO8601::Errors::UnknownPattern.new(dateTime)
+            $/x.match(date_time) or raise ISO8601::Errors::UnknownPattern.new(date_time)
 
-      @dateTime = dateTime
-      @time = @dt[6]
-      @dateSeparator = @dt[2] == @dt[4] ? @dt[2] : nil
-      @timeSeparator = 
-      if (!@dt[7].nil? and (!@dt[9].nil? and !@dt[10].nil?) and (!@dt[14].nil? and !@dt[15].nil?)) or
-         (!@dt[7].nil? and (!@dt[9].nil? and !@dt[10].nil?) and @dt[15].nil?) or
-         (!@dt[7].nil? and @dt[10].nil? and @dt[15].nil?)
-        @dt[7]
+      @date_time = date_time
+      @time = @dt[7]
+      @date_separator = @dt[3] == @dt[5] ? @dt[3] : nil
+      @time_separator = 
+      if (!@dt[8].nil? and (!@dt[10].nil? and !@dt[11].nil?) and (!@dt[15].nil? and !@dt[16].nil?)) or
+         (!@dt[8].nil? and (!@dt[10].nil? and !@dt[11].nil?) and @dt[16].nil?) or
+         (!@dt[8].nil? and @dt[11].nil? and @dt[16].nil?)
+        @dt[8]
       else
         nil
       end
-      @year = @dt[1].nil? ? nil : @dt[1].to_i
-      @month = @dt[3].nil? ? nil : @dt[3].to_i
-      @day = @dt[5].nil? ? nil : @dt[5].to_i
-      @hour = @dt[6].nil? ? nil : @dt[6].to_i
-      @minute = @dt[8].nil? ? nil : @dt[8].to_i
-      @second = @dt[10].nil? ? nil : @dt[10].to_i
+      @century = @dt[1].nil? ? nil : @dt[1].to_i
+      @year = @dt[2].nil? ? nil : (@dt[1] + @dt[2]).to_i
+      @month = @dt[4].nil? ? nil : @dt[4].to_i
+      @day = @dt[6].nil? ? nil : @dt[6].to_i
+      @hour = @dt[7].nil? ? nil : @dt[7].to_i
+      @minute = @dt[9].nil? ? nil : @dt[9].to_i
+      @second = @dt[11].nil? ? nil : @dt[11].to_i
       @timezone = {
-        :full => @dt[11],
-        :sign => @dt[12],
-        :hour => @dt[13].nil? ? nil : @dt[13].to_i,
-        :minute => @dt[15].nil? ? nil : @dt[15].to_i,
+        :full => @dt[12],
+        :sign => @dt[13],
+        :hour => @dt[14].nil? ? nil : @dt[14].to_i,
+        :minute => @dt[16].nil? ? nil : @dt[16].to_i,
       }
 
       valid_pattern?
       valid_range?
     end
     def to_time
-      Time.parse(@dateTime)
+      Time.parse(@date_time)
     end
     private
       def valid_pattern?
-        if (@dateSeparator.nil? and !@timeSeparator.nil?) or
-           (!@dateSeparator.nil? and !@time.nil? and @timeSeparator.nil? and !@minute.nil?)
-          raise ISO8601::Errors::UnknownPattern.new(@dateTime)
+        if (@date_separator.nil? and !@time_separator.nil?) or
+           (!@date_separator.nil? and !@time.nil? and @time_separator.nil? and !@minute.nil?) or
+           (@year.nil? and !@month.nil?)
+          raise ISO8601::Errors::UnknownPattern.new(@date_time)
         end
       end
       def valid_range?
         if !month.nil? and (month < 1 or month > 12)
           raise RangeError
-        elsif !day.nil? and (Time.parse(@dateTime).month != month)
+        elsif !day.nil? and (Time.parse(@date_time).month != month)
           raise RangeError
         end
       rescue ArgumentError => e
