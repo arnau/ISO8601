a55f2a8a6afbc84fa55de5245494e1a9a4c17a30
Arnau Siches <asiches@gmail.com>
2012-07-07 13:21:34 +0200

    Fix the Years#factor. It should be 0 when the atomic value is 0

--- lib/iso8601/atoms.rb
+++ lib/iso8601/atoms.rb
@@ -30,24 +30,24 @@ module ISO8601
       raise NotImplementedError, "The #factor method should be implemented for each subclass"
     end
   end
-
+  ##
   # A “calendar year” is the cyclic time interval in a calendar which is
-  # required for one revolution of the Earth around the Sun and approximated to
-  # an integral number of “calendar days”.
-
-  #A “duration year” is the duration of 365 or 366 “calendar days” depending on
-  # the start and/or the end of the corresponding time interval within the
-  # specific “calendar year”.
+  #   required for one revolution of the Earth around the Sun and approximated to
+  #   an integral number of “calendar days”.
+  #
+  # A “duration year” is the duration of 365 or 366 “calendar days” depending on
+  #   the start and/or the end of the corresponding time interval within the
+  #   specific “calendar year”.
   class Years < ISO8601::Atom
-
+    ##
     # The “duration year” average is calculated through time intervals of 400
-    # “duration years”. Each cycle of 400 “duration years” has 303 “common
-    # years” of 365 “calendar days” and 97 “leap years” of 366 “calendar days”.
+    #   “duration years”. Each cycle of 400 “duration years” has 303 “common
+    #   years” of 365 “calendar days” and 97 “leap years” of 366 “calendar days”.
     def factor
-      if @base.nil?
-        ((365 * 303 + 366 * 97) / 400) * 86400
-      elsif @atom == 0
+      if @atom == 0
         0
+      elsif @base.nil?
+        ((365 * 303 + 366 * 97) / 400) * 86400
       else
         year = (@base.year + @atom).to_i
         (Time.utc(year) - Time.utc(@base.year)) / @atom
