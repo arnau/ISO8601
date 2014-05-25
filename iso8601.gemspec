80098527914c2b5d9b4f5890e46068a75b1b4632
Arnau Siches <asiches@gmail.com>
2014-05-25 16:28:59 +0000

    Move rspec config into spec_helper

--- iso8601.gemspec
+++ iso8601.gemspec
@@ -25,6 +25,6 @@ Gem::Specification.new do |s|
 
   s.has_rdoc = 'yard'
 
-  s.add_development_dependency 'rspec', '~> 2.0'
-  s.add_development_dependency 'ZenTest', '~> 4.8'
+  s.add_development_dependency 'rspec', '~> 2.14'
+  s.add_development_dependency 'rerun'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,13 @@
 
 $:.unshift File.expand_path('..', __FILE__)
 require 'iso8601'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.color = true
+  config.order = 'random'
+  config.formatter = :documentation
+  config.fail_fast = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+end
