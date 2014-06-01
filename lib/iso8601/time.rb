f6f0422a4b318b67003d1acd33fe1c09a4057c13
Arnau Siches <asiches@gmail.com>
2014-06-01 17:32:30 +0000

    Refactor time logic into a Time class

---
+++ lib/iso8601/time.rb
@@ -0,0 +1,112 @@
+module ISO8601
+  ##
+  # A Time representation
+  #
+  # @example
+  #     t = Time.new('10:11:12')
+  #     t = Time.new('T10:11:12.5Z')
+  #     t.hour # => 10
+  #     t.minute # => 11
+  #     t.second # => 12.5
+  #     t.zone # => '+00:00'
+  class Time
+    extend Forwardable
+
+    def_delegators(:@time,
+      :to_time, :to_date, :to_datetime,
+      :hour, :minute, :zone)
+
+    ##
+    # The separator used in the original ISO 8601 string.
+    attr_reader :separator
+    ##
+    # The second atom
+    attr_reader :second
+    ##
+    # @param [String] time The time pattern
+    # @param [Date] base The base date to determine the time
+    def initialize(input, base = ::Date.today)
+      @original = input
+
+      @atoms = atomize(input)
+      @time = ::DateTime.new(*[base.year, base.month, base.day], *@atoms)
+      @second = @time.second + @time.second_fraction.to_f
+    end
+    ##
+    # Forwards the time the given amount of seconds.
+    #
+    # @param [Numeric] seconds The seconds to add
+    #
+    # @return [ISO8601::Time] New time resulting of the addition
+    def +(seconds)
+      result = (@time.to_time + seconds).iso8601.split('T')
+      ISO8601::Time.new(result.last, ::Date.parse(result.first))
+    end
+    ##
+    # Backwards the date the given amount of seconds.
+    #
+    # @param [Numeric] seconds The seconds to remove
+    #
+    # @return [ISO8601::Time] New time resulting of the substraction
+    def -(seconds)
+      result = (@time.to_time - seconds).iso8601.split('T')
+      ISO8601::Time.new(result.last, ::Date.parse(result.first))
+    end
+    ##
+    # Converts self to a time component representation.
+    def to_s
+      strf = @time.second_fraction == 0 ? 'T%H:%M:%S%Z' : 'T%H:%M:%S.%2N%Z'
+      @time.strftime(strf)
+    end
+    ##
+    # Converts self to an array of atoms.
+    def to_a
+      [hour, minute, second, zone]
+    end
+    ##
+    # The original atoms
+    def to_atoms
+      @atoms
+    end
+
+    private
+    ##
+    # Splits the time component into valid atoms.
+    # Acceptable patterns: hh, hh:mm or hhmm and hh:mm:ss or hhmmss. Any form
+    # can be prepended by `T`.
+    #
+    # @param [String] time
+    #
+    # @return [Array<Integer, Float>]
+    def atomize(input)
+      _, time, zone = /^T?(.+?)(Z|[+-].+)?$/.match(input).to_a
+
+      _, hour, separator, minute, second = /^(?:
+        (\d{2})(:?)(\d{2})\2(\d{2}(?:[.,]\d+)?) |
+        (\d{2})(:?)(\d{2}) |
+        (\d{2})
+      )$/x.match(time).to_a.compact
+
+      raise ISO8601::Errors::UnknownPattern.new(@original) if hour.nil?
+
+      @separator = separator
+
+      hour = hour.to_i
+      minute &&= minute.to_i
+      second &&= second.to_f
+
+      raise ISO8601::Errors::UnknownPattern.new(@original) unless valid_zone?(zone)
+
+      [hour, minute, second, zone].compact
+    end
+
+    def valid_zone?(zone)
+      _, offset, separator = /^(Z|[+-]\d{2}(?:(:?)\d{2})?)$/.match(zone).to_a.compact
+
+      wrong_pattern = !zone.nil? && offset.nil?
+      invalid_separators = zone.to_s.match(/^[+-]\d{2}:?\d{2}$/) && (@separator != separator)
+
+      !(wrong_pattern || invalid_separators)
+    end
+  end
+end
---
+++ spec/iso8601/time_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe ISO8601::Time do
+  it "should raise an error for any unknown pattern" do
+    expect { ISO8601::Time.new('T10:3012+0400') }.to raise_error(ISO8601::Errors::UnknownPattern)
+    expect { ISO8601::Time.new('T10:30:12+0400') }.to raise_error(ISO8601::Errors::UnknownPattern)
+    expect { ISO8601::Time.new('T10:30:12+040') }.to raise_error(ISO8601::Errors::UnknownPattern)
+  end
+
+  it "should raise an error for a correct pattern but an invalid date" do
+    expect { ISO8601::Time.new('T25:00:00') }.to raise_error(ArgumentError)
+    expect { ISO8601::Time.new('T00:61:00') }.to raise_error(ArgumentError)
+    expect { ISO8601::Time.new('T00:00:61') }.to raise_error(ArgumentError)
+  end
+
+  it "should parse any allowed pattern" do
+    expect { ISO8601::Time.new('T10') }.to_not raise_error
+    expect { ISO8601::Time.new('T10:30') }.to_not raise_error
+    expect { ISO8601::Time.new('T10:30:12') }.to_not raise_error
+    expect { ISO8601::Time.new('T10:30:12Z') }.to_not raise_error
+    expect { ISO8601::Time.new('T10:30:12+04') }.to_not raise_error
+    expect { ISO8601::Time.new('T10:30:12+04:00') }.to_not raise_error
+    expect { ISO8601::Time.new('T10:30:12-04:00') }.to_not raise_error
+    expect { ISO8601::Time.new('T103012+0400') }.to_not raise_error
+    expect { ISO8601::Time.new('T103012+04') }.to_not raise_error
+    expect { ISO8601::Time.new('T10:30:12-00:00') }.to_not raise_error
+    expect { ISO8601::Time.new('T16:26:10,5Z') }.to_not raise_error
+  end
+
+  context 'reduced patterns' do
+    it "should parse correctly reduced times" do
+      reduced_time = ISO8601::Time.new('T101112Z')
+      reduced_time.hour.should == 10
+      reduced_time.minute.should == 11
+      reduced_time.second.should == 12
+    end
+  end
+
+  it "should return each atomic value" do
+    dt = ISO8601::Time.new('T12:02:01+04:00', ::Date.parse('2010-05-09'))
+    dt.hour.should == 12
+    dt.minute.should == 2
+    dt.second.should == 1
+    dt.zone.should == '+04:00'
+  end
+
+  it "should respond to delegated casting methods" do
+    dt = ISO8601::Time.new('T10:09:08Z')
+    dt.should respond_to(:to_s, :to_time, :to_date, :to_datetime)
+  end
+
+  describe '#+' do
+    it "should return the result of the addition" do
+      (ISO8601::Time.new('T20:20:20Z') + 10).to_s.should == 'T20:20:30+00:00'
+    end
+  end
+
+  describe '#-' do
+    it "should return the result of the substraction" do
+      (ISO8601::Time.new('T20:20:20Z') - 10).to_s.should == 'T20:20:10+00:00'
+    end
+  end
+
+  describe '#to_a' do
+    it "should return an array of atoms" do
+      ISO8601::Time.new('T19:29:39Z').to_a.should == [19, 29, 39, '+00:00']
+    end
+  end
+end
