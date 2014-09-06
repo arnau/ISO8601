880c623e756e8703c728f98fa004c31b371b0725
Arnau Siches <asiches@gmail.com>
2014-09-06 13:58:59 +0100

    Change RSpec syntax for atoms

--- spec/iso8601/atoms_spec.rb
+++ spec/iso8601/atoms_spec.rb
@@ -18,29 +18,28 @@ RSpec.describe ISO8601::Atom do
     expect { ISO8601::Atom.new(1, 10) }.to raise_error
   end
   it "should create a new atom" do
-    ISO8601::Atom.new(-1).should be_an_instance_of(ISO8601::Atom)
+    expect(ISO8601::Atom.new(-1)).to be_an_instance_of(ISO8601::Atom)
   end
   describe '#to_i' do
     it "should return an integer" do
-      ISO8601::Atom.new(1).to_i.should be_a_kind_of(Integer)
-      ISO8601::Atom.new(1.0).to_i.should be_a_kind_of(Integer)
+      expect(ISO8601::Atom.new(1).to_i).to be_a_kind_of(Integer)
+      expect(ISO8601::Atom.new(1.0).to_i).to be_a_kind_of(Integer)
     end
     it "should return the integer part of the given number" do
-      n = 1.0
-      ISO8601::Atom.new(n).to_i.should == n.to_i
+      expect(ISO8601::Atom.new(1.0).to_i).to eq(1.0.to_i)
     end
   end
   describe '#to_f' do
     it "should return a float" do
-      ISO8601::Atom.new(1).to_f.should be_a_kind_of(Float)
-      ISO8601::Atom.new(1.0).to_f.should be_a_kind_of(Float)
+      expect(ISO8601::Atom.new(1).to_f).to be_a_kind_of(Float)
+      expect(ISO8601::Atom.new(1.0).to_f).to be_a_kind_of(Float)
     end
   end
   describe '#value' do
     it "should return the simplest value representation" do
-      ISO8601::Atom.new(1).value.should eq(1)
-      ISO8601::Atom.new(1.0).value.should eq(1)
-      ISO8601::Atom.new(1.1).value.should eq(1.1)
+      expect(ISO8601::Atom.new(1).value).to eq(1)
+      expect(ISO8601::Atom.new(1.0).value).to eq(1)
+      expect(ISO8601::Atom.new(1.1).value).to eq(1.1)
     end
   end
   describe '#factor' do
@@ -54,31 +53,31 @@ describe ISO8601::Years do
   describe '#factor' do
     it "should return the Year factor" do
       expect { ISO8601::Years.new(1).factor }.to_not raise_error
-      ISO8601::Years.new(2).factor.should == 31536000
-      ISO8601::Years.new(1).factor.should == 31536000
+      expect(ISO8601::Years.new(2).factor).to eq(31536000)
+      expect(ISO8601::Years.new(1).factor).to eq(31536000)
     end
     it "should return the Year factor for a common year" do
-      ISO8601::Years.new(1, ISO8601::DateTime.new("2010-01-01")).factor.should == 31536000
+      expect(ISO8601::Years.new(1, ISO8601::DateTime.new("2010-01-01")).factor).to eq(31536000)
     end
     it "should return the Year factor for a leap year" do
-      ISO8601::Years.new(1, ISO8601::DateTime.new("2000-01-01")).factor.should == 31622400
+      expect(ISO8601::Years.new(1, ISO8601::DateTime.new("2000-01-01")).factor).to eq(31622400)
     end
   end
 
   describe '#to_seconds' do
     it "should return the amount of seconds" do
-      ISO8601::Years.new(2).to_seconds.should == 63072000
-      ISO8601::Years.new(-2).to_seconds.should == -63072000
+      expect(ISO8601::Years.new(2).to_seconds).to eq(63072000)
+      expect(ISO8601::Years.new(-2).to_seconds).to eq(-63072000)
     end
     it "should return the amount of seconds for a common year" do
       base = ISO8601::DateTime.new('2010-01-01')
-      ISO8601::Years.new(2, base).to_seconds.should == 63072000
-      ISO8601::Years.new(12, base).to_seconds.should == 378691200
+      expect(ISO8601::Years.new(2, base).to_seconds).to eq(63072000)
+      expect(ISO8601::Years.new(12, base).to_seconds).to eq(378691200)
     end
     it "should return the amount of seconds for a leap year" do
       base = ISO8601::DateTime.new('2000-01-01')
-      ISO8601::Years.new(2, base).to_seconds.should == 63158400
-      ISO8601::Years.new(15, base).to_seconds.should == 473385600
+      expect(ISO8601::Years.new(2, base).to_seconds).to eq(63158400)
+      expect(ISO8601::Years.new(15, base).to_seconds).to eq(473385600)
     end
   end
 
@@ -94,38 +93,38 @@ describe ISO8601::Months do
   describe '#factor' do
     it "should return the Month factor" do
       expect { ISO8601::Months.new(1).factor }.to_not raise_error
-      ISO8601::Months.new(2).factor.should == 2628000
+      expect(ISO8601::Months.new(2).factor).to eq(2628000)
     end
     it "should return the Month factor for a common year" do
-      ISO8601::Months.new(1, ISO8601::DateTime.new('2010-01-01')).factor.should == 2678400
+      expect(ISO8601::Months.new(1, ISO8601::DateTime.new('2010-01-01')).factor).to eq(2678400)
     end
     it "should return the Month factor for a leap year" do
-      ISO8601::Months.new(1, ISO8601::DateTime.new('2000-01-01')).factor.should == 2678400
+      expect(ISO8601::Months.new(1, ISO8601::DateTime.new('2000-01-01')).factor).to eq(2678400)
     end
     it "should return the Month factor based on february for a common year" do
-      ISO8601::Months.new(1, ISO8601::DateTime.new('2010-02-01')).factor.should == 2419200
+      expect(ISO8601::Months.new(1, ISO8601::DateTime.new('2010-02-01')).factor).to eq(2419200)
     end
     it "should return the Month factor based on february for a leap year" do
-      ISO8601::Months.new(1, ISO8601::DateTime.new('2000-02-01')).factor.should == 2505600
+      expect(ISO8601::Months.new(1, ISO8601::DateTime.new('2000-02-01')).factor).to eq(2505600)
     end
   end
   describe '#to_seconds' do
     it "should return the amount of seconds" do
-      ISO8601::Months.new(2).to_seconds.should == 5256000
+      expect(ISO8601::Months.new(2).to_seconds).to eq(5256000)
     end
     it "should return the amount of seconds for a common year" do
-      ISO8601::Months.new(2, ISO8601::DateTime.new('2010-01-01')).to_seconds.should == 5097600
+      expect(ISO8601::Months.new(2, ISO8601::DateTime.new('2010-01-01')).to_seconds).to eq(5097600)
     end
     it "should return the amount of seconds for a leap year" do
-      ISO8601::Months.new(2, ISO8601::DateTime.new('2000-01-01')).to_seconds.should == 5184000
+      expect(ISO8601::Months.new(2, ISO8601::DateTime.new('2000-01-01')).to_seconds).to eq(5184000)
     end
     it "should return the amount of seconds based on februrary for a common year" do
-      ISO8601::Months.new(2, ISO8601::DateTime.new('2010-02-01')).to_seconds.should == 5097600
+      expect(ISO8601::Months.new(2, ISO8601::DateTime.new('2010-02-01')).to_seconds).to eq(5097600)
     end
     it "should return the amount of seconds based on february for a leap year" do
-      ISO8601::Months.new(2, ISO8601::DateTime.new('2000-02-01')).to_seconds.should == 5184000
-      ISO8601::Months.new(12, ISO8601::DateTime.new('2000-02-01')).to_seconds.should == 31622400
-      ISO8601::Months.new(12, ISO8601::DateTime.new('2000-02-01')).to_seconds.should == ISO8601::Years.new(1, ISO8601::DateTime.new("2000-02-01")).to_seconds
+      expect(ISO8601::Months.new(2, ISO8601::DateTime.new('2000-02-01')).to_seconds).to eq(5184000)
+      expect(ISO8601::Months.new(12, ISO8601::DateTime.new('2000-02-01')).to_seconds).to eq(31622400)
+      expect(ISO8601::Months.new(12, ISO8601::DateTime.new('2000-02-01')).to_seconds).to eq(ISO8601::Years.new(1, ISO8601::DateTime.new("2000-02-01")).to_seconds)
     end
   end
   describe '#symbol' do
@@ -139,13 +138,13 @@ end
 describe ISO8601::Weeks do
   describe '#factor' do
     it "should return the Week factor" do
-      ISO8601::Weeks.new(2).factor.should == 604800
+      expect(ISO8601::Weeks.new(2).factor).to eq(604800)
     end
   end
   describe '#to_seconds' do
     it "should return the amount of seconds" do
-      ISO8601::Weeks.new(2).to_seconds.should == 1209600
-      ISO8601::Weeks.new(-2).to_seconds.should == -1209600
+      expect(ISO8601::Weeks.new(2).to_seconds).to eq(1209600)
+      expect(ISO8601::Weeks.new(-2).to_seconds).to eq(-1209600)
     end
   end
   describe '#symbol' do
@@ -159,11 +158,11 @@ end
 describe ISO8601::Days do
   describe '#factor' do
     it "should return the Day factor" do
-      ISO8601::Days.new(2).factor.should == 86400
+      expect(ISO8601::Days.new(2).factor).to eq(86400)
     end
     it "should return the amount of seconds" do
-      ISO8601::Days.new(2).to_seconds.should == 172800
-      ISO8601::Days.new(-2).to_seconds.should == -172800
+      expect(ISO8601::Days.new(2).to_seconds).to eq(172800)
+      expect(ISO8601::Days.new(-2).to_seconds).to eq(-172800)
     end
   end
   describe '#symbol' do
@@ -177,11 +176,11 @@ end
 describe ISO8601::Hours do
   describe '#factor' do
     it "should return the Hour factor" do
-      ISO8601::Hours.new(2).factor.should == 3600
+      expect(ISO8601::Hours.new(2).factor).to eq(3600)
     end
     it "should return the amount of seconds" do
-      ISO8601::Hours.new(2).to_seconds.should == 7200
-      ISO8601::Hours.new(-2).to_seconds.should == -7200
+      expect(ISO8601::Hours.new(2).to_seconds).to eq(7200)
+      expect(ISO8601::Hours.new(-2).to_seconds).to eq(-7200)
     end
   end
   describe '#symbol' do
@@ -195,11 +194,11 @@ end
 describe ISO8601::Minutes do
   describe '#factor' do
     it "should return the Minute factor" do
-      ISO8601::Minutes.new(2).factor.should == 60
+      expect(ISO8601::Minutes.new(2).factor).to eq(60)
     end
     it "should return the amount of seconds" do
-      ISO8601::Minutes.new(2).to_seconds.should == 120
-      ISO8601::Minutes.new(-2).to_seconds.should == -120
+      expect(ISO8601::Minutes.new(2).to_seconds).to eq(120)
+      expect(ISO8601::Minutes.new(-2).to_seconds).to eq(-120)
     end
   end
   describe '#symbol' do
@@ -213,11 +212,11 @@ end
 describe ISO8601::Seconds do
   describe '#factor' do
     it "should return the Second factor" do
-      ISO8601::Seconds.new(2).factor.should == 1
+      expect(ISO8601::Seconds.new(2).factor).to eq(1)
     end
     it "should return the amount of seconds" do
-      ISO8601::Seconds.new(2).to_seconds.should == 2
-      ISO8601::Seconds.new(-2).to_seconds.should == -2
+      expect(ISO8601::Seconds.new(2).to_seconds).to eq(2)
+      expect(ISO8601::Seconds.new(-2).to_seconds).to eq(-2)
     end
   end
   describe '#symbol' do
@@ -227,4 +226,3 @@ describe ISO8601::Seconds do
     end
   end
 end
-
--- spec/iso8601/date_spec.rb
+++ spec/iso8601/date_spec.rb
@@ -38,47 +38,44 @@ describe ISO8601::Date do
   context 'reduced patterns' do
     it "should parse correctly reduced dates" do
       reduced_date = ISO8601::Date.new('20100509')
-      reduced_date.year.should == 2010
-      reduced_date.month.should == 5
-      reduced_date.day.should == 9
+      expect(reduced_date.year).to eq(2010)
+      expect(reduced_date.month).to eq(5)
+      expect(reduced_date.day).to eq(9)
     end
   end
 
   it "should return the right sign for the given year" do
-    ISO8601::Date.new('-2014-05-31').year.should == -2014
-    ISO8601::Date.new('+2014-05-31').year.should == 2014
+    expect(ISO8601::Date.new('-2014-05-31').year).to eq(-2014)
+    expect(ISO8601::Date.new('+2014-05-31').year).to eq(2014)
   end
 
   it "should respond to delegated casting methods" do
-    dt = ISO8601::Date.new('2014-12-11')
-    dt.should respond_to(:to_s, :to_time, :to_date, :to_datetime)
+    expect(ISO8601::Date.new('2014-12-11')).to respond_to(:to_s, :to_time, :to_date, :to_datetime)
   end
 
   describe '#+' do
     it "should return the result of the addition" do
-      (ISO8601::Date.new('2012-07-07') + 7).to_s.should == '2012-07-14'
+      expect((ISO8601::Date.new('2012-07-07') + 7).to_s).to eq('2012-07-14')
     end
   end
 
   describe '#-' do
     it "should return the result of the substraction" do
-      (ISO8601::Date.new('2012-07-07') - 7).to_s.should == '2012-06-30'
+      expect((ISO8601::Date.new('2012-07-07') - 7).to_s).to eq('2012-06-30')
     end
   end
 
   describe '#to_a' do
     it "should return an array of atoms" do
-      dt = ISO8601::Date.new('2014-05-31').to_a
-      dt.should be_kind_of(Array)
-      dt.should == [2014, 5, 31]
+      expect(ISO8601::Date.new('2014-05-31').to_a).to eq([2014, 5, 31])
     end
   end
 
   describe '#atoms' do
     it "should return an array of original atoms" do
-      ISO8601::Date.new('2014-05-02').atoms.should == [2014, 5, 2]
-      ISO8601::Date.new('2014-05').atoms.should == [2014, 5]
-      ISO8601::Date.new('2014').atoms.should == [2014]
+      expect(ISO8601::Date.new('2014-05-02').atoms).to eq([2014, 5, 2])
+      expect(ISO8601::Date.new('2014-05').atoms).to eq([2014, 5])
+      expect(ISO8601::Date.new('2014').atoms).to eq([2014])
     end
   end
 
